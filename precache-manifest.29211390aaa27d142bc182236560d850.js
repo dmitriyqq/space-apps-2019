9a1f46d60d787845916f36989e16aa8c37d5ec1b
Dmitrii <dmitrii.shulaikin@akvelon.com>
2019-10-21 12:02:48 +0300

    Updates

---
+++ precache-manifest.29211390aaa27d142bc182236560d850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985f3d3f1049db398abbe8ff5e401fee",
+    "url": "/spaceapps2019/index.html"
+  },
+  {
+    "revision": "a60d4748afcaeed9436a",
+    "url": "/spaceapps2019/static/css/main.89ef7d4d.chunk.css"
+  },
+  {
+    "revision": "52675372972f16c1e867",
+    "url": "/spaceapps2019/static/js/2.0e8044d0.chunk.js"
+  },
+  {
+    "revision": "a60d4748afcaeed9436a",
+    "url": "/spaceapps2019/static/js/main.ef7023c6.chunk.js"
+  },
+  {
+    "revision": "40f93423426cb421a48d",
+    "url": "/spaceapps2019/static/js/runtime-main.9ec96281.js"
+  },
+  {
+    "revision": "2c8efb484ba2c6421ac540a0020d71ca",
+    "url": "/spaceapps2019/static/media/Gorod.2c8efb48.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/spaceapps2019/static/media/logo.25bf045c.svg"
+  }
+]);
